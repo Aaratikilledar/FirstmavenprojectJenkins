223cb8671750a8f9192ace3b6706847fff525695
Aarati <aarati@gmail.com>
2024-07-25 19:49:22 +0530

    first commit

---
+++ src/main/java/com/igap/FirstMavenproject/FirstMavenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.igap.FirstMavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMavenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igap/FirstMavenproject/FirstMavenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igap.FirstMavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMavenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
